b4af8e36ba61a8d8d261876311dc622ea8fada96
nolu-baloyi <ST10478159@vcconnect.edu.za>
2026-06-10 09:57:41 +0200

    Splash screen layout
    this has the ui elements for the splash screen including background drawable and image

---
+++ imad5112examlinknolubaloyi/app/src/main/java/com/example/imad5112_exam_link_nolu_baloyi/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.imad5112_exam_link_nolu_baloyi
+
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var image : ImageView
+    private lateinit var title : TextView
+    private lateinit var progressBar : ProgressBar
+    
+    }
