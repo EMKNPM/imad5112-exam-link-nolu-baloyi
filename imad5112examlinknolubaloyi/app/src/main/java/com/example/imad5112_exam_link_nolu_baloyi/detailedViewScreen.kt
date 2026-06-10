bd3f977dd9a691e17dfd284c5f66a50ec923a258
nolu-baloyi <ST10478159@vcconnect.edu.za>
2026-06-10 13:54:43 +0200

    Update detailedViewScreen.kt

--- imad5112examlinknolubaloyi/app/src/main/java/com/example/imad5112_exam_link_nolu_baloyi/detailedViewScreen.kt
+++ imad5112examlinknolubaloyi/app/src/main/java/com/example/imad5112_exam_link_nolu_baloyi/detailedViewScreen.kt
@@ -1,15 +1,15 @@
 package com.example.imad5112_exam_link_nolu_baloyi
 
 import android.os.Bundle
+import android.util.Log.i
+import android.view.Gravity
+import android.view.View
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.ScrollView
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class detailedViewScreen : AppCompatActivity() {
     //declarations
@@ -33,25 +33,34 @@ class detailedViewScreen : AppCompatActivity() {
         }
     }
 
-    private fun displayGearList() {
+    fun createGearCard(itemNames: Any, categories: Any, quantities: Any, comments: Any) {
+        TODO("Not yet implemented")
+    }
+
+    private fun view(screen: detailedViewScreen) {
+        TODO("Not yet implemented")
+    }
+
+    private fun displayGearList(darker_gray: Unit.() -> Int, isEmpty: Any.() -> Boolean) {
         gearListContainer.removeAllViews()
 
         //using loop to display all gear items
-        for (i < MainActivity.itemNames.indices) {
-            val gearCard = createGearCard(
-                MainActivity.itemNames[i],
-                MainActivity.categories[i],
-                MainActivity.quantities[i],
-                MainActivity.comments[i]
+        for (i < MainActivity.itemNames) {
+            val gearCard: View? = createGearCard(
+                MainActivity.itemNames,
+                MainActivity.categories,
+                MainActivity.quantities,
+                MainActivity.comments
             )
             gearListContainer.addView(gearCard)}
 
             //adding divider between items
-            if (i < MainActivity.itemNames.size - 1) {
+            if (i < MainActivity.itemNames.toString() - 1) {
                 val divider = view(this).apply{
+                    val layoutParams = null
                     layoutParams.MATCH_PARENT, 2
                     )
-                    setBackgroundColor(ContextCompat.getColor(context, android.R.color.darker_gray()))
+                    setBackgroundColor(ContextCompat.getColor(context(), android.R.color.darker_gray()))
                 }
             }
             //show message if no items
@@ -60,12 +69,12 @@ class detailedViewScreen : AppCompatActivity() {
                     text = "no gear items added yet.\nTap  'add gear' to get started!"
                     textSize = 16f
                     setPadding((50, 100, 50, 100)
-                    gravity = android.view.Gravity.CENTER)
+                    gravity = Gravity.CENTER)
 
                 }
             }
 
-            private fun createGearCard(itemName: ParameterName)
+
         }
     }
 
